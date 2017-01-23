89adcacaf6dca182d436833d3bd80b6e3fbb90b4
Admin-PC\Admin <qsef0987@naver.com>
2017-01-23 16:58:44 +0900

    올라가나?

--- OOP/src/com/ktsd/ehm/Student.java
+++ OOP/src/com/ktsd/ehm/Student.java
@@ -1,13 +1,42 @@
 package com.ktsd.ehm;
 
 public class Student {
-
-	String name = "엄혜미";
-	int grade = 3;
-	int classNumber = 15;
-	String schoolName = "답십리초";
-	String[] books = {"C를 배운적이 없다구요","JAVA를 배운적이 없다구요","C++를 배운적이 없다구요"};
-	String[] subjects = {"국어","영어","수학"};
+	//멤버변수는 private로 사용
+	private String name = "엄혜미";
+	private int grade = 3;
+	private int classNumber = 15;
+	private String schoolName = "답십리초";
+	private String[] books = {"C를 배운적이 없다구요","JAVA를 배운적이 없다구요","C++를 배운적이 없다구요"};
+	private String[] subjects = {"국어","영어","수학"};
+	
+	//instance field에 대해서 setter생성
+	public void setName(String name){
+		//instance field의 변수에 접근시 this
+		//파라미터로 전달된 데이터 길이가 3이면 변경하기
+		if(name.length() == 3){
+			this.name = name;
+		}
+	
+	}
+	public void setgrade(int grade){
+		this.grade = grade;
+	}
+	public void setClassNumber(int classNumber){
+		this.classNumber = classNumber;
+	}
+	public void setSchoolName(String schoolName){
+		this.schoolName = schoolName;
+	}
+	public void setBooks(String[] books){
+		this.books = books;
+	}
+	public void setSubjects(String[] subjects){
+		this.subjects = subjects;
+	}
+	
+	void introduce(){
+		System.out.printf("제 이름은 [%s] 입니다.\n",name);
+	}
 	
 	void study(){
 		for (int i = 0; i < subjects.length; i++) {
--- OOP/src/com/ktsd/ehm/StudentMain.java
+++ OOP/src/com/ktsd/ehm/StudentMain.java
@@ -3,10 +3,17 @@ package com.ktsd.ehm;
 public class StudentMain {
 
 	public static void main(String[] args){
-		Student student = new Student();
-		student.readBooks();
-		student.study();
-		student.takeExam();
+		Student ehm = new Student();
+		
+		ehm.setName("혜미");
+		String[] books = {"새책1","새책2","새책3"};
+		String[] subjects = {"새과목1","새과목2","새과목3"};
+		
+		ehm.introduce();
+		ehm.setBooks(books);
+		ehm.setSubjects(subjects);
+		ehm.readBooks();
+		ehm.study();
 		
 	}
 }
