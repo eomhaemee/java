124e9c7fef444980c70250328541232d188386b6
Admin-PC\Admin <qsef0987@naver.com>
2017-02-09 18:05:10 +0900

    2017.02.09

--- JDBC_Test/src/com/ktds/ehm/hr/dao/HRDao2.java
+++ JDBC_Test/src/com/ktds/ehm/hr/dao/HRDao2.java
@@ -2,12 +2,13 @@ package com.ktds.ehm.hr.dao;
 
 import java.util.List;
 
-import com.ktds.ehm.hr.vo.CountriesVO;
 import com.ktds.ehm.hr.vo.EmployeesVO;
 
 public interface HRDao2 {
 
 	public List<EmployeesVO> getAllEmployees();
+	
+	public EmployeesVO findOneEmployee(int employeeId);
 
 	
 }
--- JDBC_Test/src/com/ktds/ehm/hr/dao/HRDao2Impl.java
+++ JDBC_Test/src/com/ktds/ehm/hr/dao/HRDao2Impl.java
@@ -38,7 +38,7 @@ public class HRDao2Impl extends JDBCDaoSupport implements HRDao2 {
 
 			@Override
 			public void mappingParameters(PreparedStatement stmt) throws SQLException {
-				// TODO Auto-generated method stub
+			
 
 			}
 
@@ -51,5 +51,51 @@ public class HRDao2Impl extends JDBCDaoSupport implements HRDao2 {
 		});
 
 	}
+
+	@Override
+	public EmployeesVO findOneEmployee(int employeeId) {
+		
+
+		return (EmployeesVO) selectOne(new QueryHandler() {
+			
+			@Override
+			public String preparedQuery() {
+				StringBuffer query = new StringBuffer();
+				
+				query.append("	SELECT	EMPLOYEE_ID      ");
+				query.append("	        ,FIRST_NAME      ");
+				query.append("	        ,LAST_NAME       ");
+				query.append("	        ,EMAIL           ");
+				query.append("	        ,PHONE_NUMBER    ");
+				query.append("	        ,HIRE_DATE       ");
+				query.append("	        ,JOB_ID          ");
+				query.append("	        ,SALARY          ");
+				query.append("	        ,COMMISSION_PCT  ");
+				query.append("	        ,MANAGER_ID      ");
+				query.append("	        ,DEPARTMENT_ID   ");
+				query.append(" FROM		EMPLOYEES        ");
+				query.append(" WHERE	EMPLOYEE_ID = ?  ");
+				
+				return query.toString();
+			}
+			
+			@Override
+			public void mappingParameters(PreparedStatement stmt) throws SQLException {
+				stmt.setInt(1, employeeId);
+				
+			}
+			
+			@Override
+			public Object getData(ResultSet rs) {
+				EmployeesVO employeesVO = new EmployeesVO();
+				BindData.bindData(rs, employeesVO);
+				return employeesVO;
+			}
+		});
+		
+		
+		
+	}
+
 
 }
